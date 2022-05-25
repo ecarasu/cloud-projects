0b5aacae6ce30466cdd66687ffbb209d0dda502c
Arasu Mathivanan <39849732+ecarasu@users.noreply.github.com>
2022-05-25 10:21:36 +0530

    Update config.py

--- Python/ocs_gcp_migration/config.py
+++ Python/ocs_gcp_migration/config.py
@@ -1,8 +1,8 @@
 oci_bucket_name = 'gcp_migration'
 gcp_bucket_name = 'oci-migration-2022'
 download_dir = f'D:{chr(92)}{oci_bucket_name}'
-gcs_service_account_credentials = r'D:\Career\Playground\GCP\triple-shift-350411-72c624b2f9b9.json'
-log_file = r'D:\Career\Learning\Python\Projects\OCI_GCP\Logs\Migration.log'
+gcs_service_account_credentials = r''
+log_file = r''
 chunk_size = 2048 ** 2
 audit_header = ['OCS_FILE_NAME',
                 'SIZE',
@@ -11,4 +11,4 @@ audit_header = ['OCS_FILE_NAME',
                 'DOWNLOAD_END_DATE',
                 'DOWNLOAD_END_TIME',
                 'DOWNLOAD_PERCENTAGE']
-audit_file = r'D:\Career\Learning\Python\Projects\OCI_GCP\Audit.csv'
+audit_file = r''
