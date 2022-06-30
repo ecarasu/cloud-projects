02f8f508a3c664237dd1d952c47e12b7ee7eaba2
Arasu Mathivanan <39849732+ecarasu@users.noreply.github.com>
2022-06-30 13:32:12 +0530

    Add files via upload

---
+++ Python/ocs_gcp_migration/upload_to_ocs.py
@@ -0,0 +1,22 @@
+import requests
+
+
+with open(r'D:\Career\AWS.pdf', 'rb') as f:
+    data = f.read()
+
+base_url = 'https://objectstorage.us-ashburn-1.oraclecloud.com'
+
+# pre auth url
+url = 'https://objectstorage.us-ashburn-1.oraclecloud.com/p/a-kmzCF7mlE6FW73SJmOFE2MaN4CCv4CTfafrrBJZMm5BeOZihQqbB6iuGsqubar/n/idt88w6rh4ji/b/gcp_migration/o/Career/AWS.pdf'
+headers = {'opc-multipart': 'true'}
+
+get_multipart_url = requests.put(url, headers=headers)
+response = get_multipart_url.json()
+
+# putting object in multipart URL
+requests.put(url=base_url+response['accessUri']+'1', data=data)
+
+# commiting multi part upload
+result = requests.post(url=base_url+response['accessUri'])
+
+print(result.status_code)
